68e528daf91b4df9bc4f5da48569545ee47b901f
deburau <deburau@users.noreply.github.com>
2021-04-14 14:03:30 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+cd /var/www/html
+
+if [ -z "$(ls)" ]
+then
+    echo "unpacking pagekit archive"
+    unzip -q /opt/pagekit.zip
+    rm /opt/pagekit.zip
+    chown -R www-data:www-data .
+    chmod -R g+w .
+    chmod +x pagekit
+fi
+
+if [ ! -f config.php ]
+then
+    if [ -n "$PAGEKIT_USERNAME$PAGEKIT_PASSWORD$PAGEKIT_TITLE$PAGEKIT_MAIL$PAGEKIT_DB_DRIVER$PAGEKIT_DB_PREFIX$PAGEKIT_DB_HOST$PAGEKIT_DB_NAME$PAGEKIT_DB_USERNAME$PAGEKIT_DB_PASSWORD$PAGEKIT_LOCALE" ]
+    then
+        echo "executing pagekit setup via CLI"
+        ./pagekit setup -vvv --no-interaction \
+                  ${PAGEKIT_USERNAME:+"--username=${PAGEKIT_USERNAME}"} \
+                  ${PAGEKIT_PASSWORD:+"--password=${PAGEKIT_PASSWORD}"} \
+                  ${PAGEKIT_TITLE:+"--title=${PAGEKIT_TITLE}"} \
+                  ${PAGEKIT_MAIL:+"--mail=${PAGEKIT_MAIL}"} \
+                  ${PAGEKIT_DB_DRIVER:+"--db-driver=${PAGEKIT_DB_DRIVER}"} \
+                  ${PAGEKIT_DB_PREFIX:+"--db-prefix=${PAGEKIT_DB_PREFIX}"} \
+                  ${PAGEKIT_DB_HOST:+"--db-host=${PAGEKIT_DB_HOST}"} \
+                  ${PAGEKIT_DB_NAME:+"--db-name=${PAGEKIT_DB_NAME}"} \
+                  ${PAGEKIT_DB_USERNAME:+"--db-user=${PAGEKIT_DB_USERNAME}"} \
+                  ${PAGEKIT_DB_PASSWORD:+"--db-pass=${PAGEKIT_DB_PASSWORD}"} \
+                  ${PAGEKIT_LOCALE:+"--locale=${PAGEKIT_LOCALE}"}
+        chown -R www-data:www-data .
+    else
+        echo "visit your new pagekit site to finish the setup"
+    fi
+fi
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- apache2-foreground "$@"
+fi
+
+exec "$@"
